2ede219689b100ebbcc3967bf79ae2636744a774
kingBook <4121266604@qq.com>
2025-04-09 16:09:27 +0800

    create3

--- src/SpinePlayer.ts
+++ src/SpinePlayer.ts
@@ -5,11 +5,9 @@ export class SpinePlayer extends Laya.Script {
     declare owner: Laya.Sprite | Laya.Sprite3D;
     private _spine: Laya.Spine2DRenderNode;
     onStart(): void {
-        Laya.timer.frameOnce(2, this, () => {
+        Laya.timer.once(1000, this, () => {
             this._spine = this.owner.getComponent(Laya.Spine2DRenderNode);
             this._spine.play("run", true);
-            console.log(this._spine.animationName);
-            console.log(this._spine.getAnimNum());
         });
     }
 }
