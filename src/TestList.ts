9c257ed8cb7a70da1924d4ba540b7379ca9a22a6
pq <pq99880022@gmail.com>
2025-07-26 20:03:31 +0800

    update4

--- src/TestList.ts
+++ src/TestList.ts
@@ -8,18 +8,15 @@ export class TestList extends Laya.Script {
 
     onAwake(): void {
         console.log("set itemRenderer");
-        
-        this.list2.numItems=5;
 
-        //this.list2.itemRenderer = this.onRenderList2Item;
-        this.list2.itemRenderer = (index: number, item: any)=>{
-             console.log("index:",index, "item:",item);
-        };
+        this.list2.itemRenderer = this.onRenderList2Item;
+        this.list2.numItems = 5;
+
     }
 
     onRenderList2Item(index: number, item: any): void {
-        console.log("index:",index, "item:",item);
-        
+        console.log("index:", index, "item:", item);
+
     }
 
 }
